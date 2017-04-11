79d2b6fefd2bb55306d47cbf4b1c582469ed2d97
Laura Goldstein <laura.koss.goldstein@gmail.com>
2017-04-11 16:38:02 -0400

    scroll to and about text

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("hello world");
+$(window).scroll(function(){
+  var sticky = $('header'),
+      scroll = $(window).scrollTop();
+
+  if (scroll >= 100) sticky.addClass('fixed');
+  else sticky.removeClass('fixed');
+});
